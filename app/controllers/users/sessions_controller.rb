a11d6305c737f2f3e00d8a30139eb5611e317e11
Aastha280301 <aamujawdiya@bestpeers.com>
2023-11-20 13:24:47 +0530

    "updation in secret key"

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -16,7 +16,7 @@ class Users::SessionsController < Devise::SessionsController
   end
   def respond_to_on_destroy
     if request.headers['Authorization'].present?
-      jwt_payload = JWT.decode(request.headers['Authorization'].split(' ').last, Rails.application.credentials.devise_jwt_secret_key!).first
+      jwt_payload = JWT.decode(request.headers['Authorization'].split(' ').last, Rails.application.credentials.devise_jwt_secret_key!).first || 9okDHL8a09X9TkEjAO6frTNpgxPvA6GQZmKunOnhZJlQVnWOyvDM2Z0WkTCtNpp7I8/CROWTqK/p719PMo04NQFixJ9M9KCKVnXPPNA4j1n/fC8usghXB/wEnNK+1QNWxZts5xRszvihfZrxtKH3cVvl7+5fQ5wKX4+orFo+bYaNbbEE4TjGWeB/CfeDpDEXhXCEZyGP1HH4aGwSdRFLYgJmB3Edom6/NQHjcip3DE2m8vkMM/abB1mLJ6RQ6NL8oQoywvzKNg6Wl7PggLmnY033G+MiN5GViWKfxK+cJRS+GNIOFRenrzIifaNlspSwjwAoB5SzolafALVs/Z+3/c+yNvUfNa7hB/i0D9OLwsqnzrEZIrNkVWS1MavCKm7dXFbFTpsLuufMri33t7GRBjntbMBspURHAIzPSw==--1tMx/yhhspnE6LEf--0KeD9R8nkSr1FNPEx36AAg==
       current_user = User.find(jwt_payload['sub'])
     end
     
